557c96193cac05179630b50c2b8025ff453c8a80
Abhilash-0322 <mauryaabhi2003@gmail.com>
2026-08-24 01:23:29 +0530

    not_dividing_900

---
+++ B_Not_Dividing.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;
+        cin>>n;
+        
+        vector<int>arr(n);
+        
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+            if(arr[i]==1){
+                arr[i]=2;
+            }
+        }
+        
+        // sort(arr.begin(),arr.end());
+        
+        // if(arr[0]==1){
+        //     arr[0]=2;
+        // }
+        
+        for(int i=1;i<n;i++){
+            if(arr[i]%arr[i-1]==0){
+                // if(arr[i-1]==1){
+                //     arr[i-1]+=1;
+                //     arr[i]+=1;
+                // }
+                // else{
+                //     arr[i]+=1;
+                // }
+                arr[i]++;
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            cout<<arr[i]<<" ";
+        }
+        
+        cout<<endl;
+    }
+}
